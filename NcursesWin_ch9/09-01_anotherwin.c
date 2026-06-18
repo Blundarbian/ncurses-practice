cfdc40c4fdfdb92ad90db9ab2bcdff92eed3477d
Blundarbian <Jambo3657@gmail.com>
2026-06-17 20:03:25 -0700

    additional windows

---
+++ NcursesWin_ch9/09-01_anotherwin.c
@@ -0,0 +1,28 @@
+#include <ncurses.h>
+
+int main() 
+{
+	WINDOW *another;
+
+	initscr();
+
+	addstr("This is the standard screen\n");
+	refresh();
+	getch();
+
+	// create another window
+	another = newwin(0, 0, 0, 0);
+	if (another == NULL)
+	{
+		addstr("unable to create window");
+		refresh();
+		getch();
+
+	} else {
+		waddstr(another, "This is another window");
+		wrefresh(another);
+		wgetch(another);
+	}
+	endwin();
+	return 0;
+}
---
+++ NcursesWin_ch9/09-02_switch.c
@@ -0,0 +1,37 @@
+#include <ncurses.h>
+
+int main()
+{
+	WINDOW *second;
+
+	initscr();
+	start_color();
+
+	init_pair(1, COLOR_WHITE, COLOR_BLUE);
+	init_pair(2, COLOR_WHITE, COLOR_RED);
+
+	second = newwin(0, 0, 0, 0);
+	if (second == NULL)
+	{
+		endwin();
+		puts("Unable to create window");
+		return 1;
+	}
+	wbkgd(second, COLOR_PAIR(2));
+	waddstr(second, "This is the second window\n");
+
+
+	// stdscr
+	bkgd(COLOR_PAIR(1));
+	addstr("This is the standard screen\n");
+	addstr("Press Enter");
+	refresh();
+	getch();
+
+	//second scr
+	wrefresh(second);
+	getch();
+
+	endwin();
+	return 0;
+}
